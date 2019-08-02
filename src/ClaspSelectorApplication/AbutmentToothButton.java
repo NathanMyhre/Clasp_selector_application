17518af17e3dd5429ac68f6b218579b64f2c378e
NathanMyhre <37623148+NathanMyhre@users.noreply.github.com>
2019-08-02 13:50:37 -0500

    Figured out potential way to paint over teeth. Not much change.

--- src/ClaspSelectorApplication/AbutmentToothButton.java
+++ src/ClaspSelectorApplication/AbutmentToothButton.java
@@ -3,6 +3,28 @@ package ClaspSelectorApplication;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseMotionListener;
+import java.awt.event.MouseMotionAdapter;
+
+
+/*
+   NOTE: Change AbutmentToothButton to a new class that embeds an image.
+   We can use the example in SwingPainting.java to extend a JPanel and custom paint
+   shapes over the image to select teeth. Define teeth as having specific coordinates
+   and implement a mouse Listener to pass coordinates of a click to a method which compares
+   the passed coordinates to the set ones. If equal to one of them, do some logic - e.g.
+   pass the tooth information to GUI, informing Tooth Type and auto-selecting.
+
+   To test, I can use a mouse listener and print out the coordinates to the terminal.
+
+   Highly recommended to finish the custom painting tutorial in Swing Trail.
+ */
 
 public class AbutmentToothButton extends JButton implements ActionListener {
     ClaspGUI gui;
@@ -40,6 +62,49 @@ public class AbutmentToothButton extends JButton implements ActionListener {
             System.err.println("Couldn't find file: " + path);
             return null;
         }
+    }
+
+}
+
+/**
+ * Opaque red circle used to highlight selected teeth
+ */
+class RedCircle {
+    private int xPos = 50;
+    private int yPos = 50;
+    private int width = 20;
+    private int height = 20;
+
+    public void setX(int xPos){
+        this.xPos = xPos;
+    }
+
+    public int getX(){
+        return xPos;
+    }
+
+    public void setY(int yPos){
+        this.yPos = yPos;
+    }
+
+    public int getY(){
+        return yPos;
+    }
+
+    public int getWidth(){
+        return width;
+    }
+
+    public int getHeight(){
+        return height;
+    }
+
+    public void paintCircle(Graphics g){
+        Color transRed = new Color(1,0,0, 0.5f);
+        g.setColor(transRed);
+        g.fillOval(xPos,yPos,width,height);
+        g.setColor(Color.BLACK);
+        g.drawRect(xPos,yPos,width,height);
     }
 
 }
